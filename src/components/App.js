859710730cc1bc21d5f6d3f02cb84597edb31faa
Leandro Pastor de Latorre <pastordl.leandro@gmail.com>
2020-11-03 00:04:12 -0300

    agrego AprobarReclamoScreen

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,53 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import HomeScreen from './home/HomeScreen';
-import ReclamosScreen from './reclamos/ReclamosScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./home/HomeScreen";
+import ReclamosScreen from "./reclamos/ReclamosScreen";
+import { Provider as PaperProvider } from 'react-native-paper';
+import AprobarReclamoScreen from "./reclamos/AprobarReclamoScreen";
 
 const loggedUserInfo = {
   nombre: "Sebastian",
   apellido: "Monti",
   dni: 36826858,
   reclamos: [
-      {
-          id: 1,
-          titulo: "Boton ascensor roto 5to piso",
-          descripcion: "Se encontró que el boton del 5to piso no está funcionando, impidiendo solicitar el ascensor",
-          estado: "INSPECCIÓN"
-      }
-  ]
-}
+    {
+      id: 1,
+      titulo: "Boton ascensor roto 5to piso",
+      descripcion:
+        "Se encontró que el boton del 5to piso no está funcionando, impidiendo solicitar el ascensor",
+      estado: "INSPECCIÓN",
+    },
+  ],
+};
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
+      <PaperProvider>
+        {/* <NavigationContainer>
         <Stack.Navigator initialRouteName="Reclamos">
           <Stack.Screen name="Home" options={{ title: "Administrador de consorcios" }}>
             {props => <HomeScreen {...props} userInfo={loggedUserInfo} />}
           </Stack.Screen>
           <Stack.Screen name="Reclamos" component={ReclamosScreen} options={{ title: "Lista de reclamos" }} />
         </Stack.Navigator>
-      </NavigationContainer>
+      </NavigationContainer> */}
+        <AprobarReclamoScreen />
         <StatusBar style="auto" />
+      </PaperProvider>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-        container: {
-        flex: 1,
+  container: {
+    flex: 1,
     flexDirection: "column",
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
---
+++ src/components/reclamos/AprobarReclamoScreen.js
@@ -0,0 +1,196 @@
+import React from "react";
+import { View, StyleSheet} from "react-native";
+import {
+  TextInput,
+  Button,
+  Dialog,
+  Portal,
+} from "react-native-paper";
+
+const AprobarReclamoScreen = () => {
+  const [visibleAprobar, setVisibleAprobar] = React.useState(false);
+  const showDialogAprobar = () => setVisibleAprobar(true);
+  const hideDialogAprobar = () => setVisibleAprobar(false);
+
+  const [visibleRechazar, setVisibleRechazar] = React.useState(false);
+  const showDialogRechazar = () => setVisibleRechazar(true);
+  const hideDialogRechazar = () => setVisibleRechazar(false);
+
+  const [textTipoReclamo, setTextTipoReclamo] = React.useState("");
+  const [textPiso, setTextPiso] = React.useState("");
+  const [textEdificio, setTextEdificio] = React.useState("");
+  const [textDptoArea, setTextDptoArea] = React.useState("");
+  const [textReclamo, setTextReclamo] = React.useState("");
+
+  return (
+    <View>
+      <TextInput
+        label="Tipo de reclamo"
+        value={textTipoReclamo}
+        onChangeText={(textTipoReclamo) => setTextTipoReclamo(textTipoReclamo)}
+        style={styles.text}
+        disabled="true"
+      />
+
+      <TextInput
+        label="Piso"
+        value={textPiso}
+        onChangeText={(textPiso) => setTextPiso(text)}
+        style={styles.text}
+        disabled="true"
+      />
+
+      <TextInput
+        label="Edificio"
+        value={textEdificio}
+        onChangeText={(textEdificio) => setTextEdificio(textEdificio)}
+        style={styles.text}
+        disabled="true"
+      />
+
+      <TextInput
+        label="Departamento, área común, etc"
+        value={textDptoArea}
+        onChangeText={(textDptoArea) => setTextDptoArea(textDptoArea)}
+        style={styles.text}
+        disabled="true"
+      />
+
+      <TextInput
+        label="Descripción del reclamo"
+        value={textReclamo}
+        onChangeText={(textReclamo) => setTextReclamo(textReclamo)}
+        style={styles.textReclamo}
+        numberOfLines={20}
+        multiline={true}
+        disabled="true"
+      />
+
+      {/* Pendientes
+      2) Falta agregar el componente para adjuntar imágenes (https://github.com/react-native-image-picker/react-native-image-picker)
+      */}
+
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            mode="contained"
+            color="green"
+            style={styles.buttons}
+            // onPress={() => console.log("Pressed")}
+            onPress={showDialogAprobar}
+          >
+            Aprobar
+          </Button>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            mode="contained"
+            color="red"
+            style={styles.buttons}
+            onPress={showDialogRechazar}
+          >
+            Rechazar
+          </Button>
+        </View>
+      </View>
+      
+      
+      <Portal>
+        <Dialog visible={visibleAprobar} onDismiss={hideDialogAprobar}>
+          <Dialog.Title>Fecha de resolución</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+            // label="Email"
+            // value={text}
+            // onChangeText={(text) => setText(text)}
+            style={styles.fechaResolucionReclamo}
+            />
+          </Dialog.Content>
+          <Dialog.Title>Comentario</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+            // label="Email"
+            // value={text}
+            // onChangeText={(text) => setText(text)}
+            style={styles.comentarioResolucionReclamo}
+            multiline={true}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialogAprobar}>Cancel</Button>
+            <Button onPress={() => console.log("Ok")}>Ok</Button>
+          </Dialog.Actions>
+        </Dialog>
+
+        <Dialog visible={visibleRechazar} onDismiss={hideDialogRechazar}>
+          <Dialog.Title>Motivo de rechazo</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+            // label="Email"
+            // value={text}
+            // onChangeText={(text) => setText(text)}
+            style={styles.comentarioResolucionReclamo}
+            multiline={true}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialogRechazar}>Cancel</Button>
+            <Button onPress={() => console.log("Ok")}>Ok</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: "red",
+    fontWeight: "bold",
+    textAlignVertical: "center",
+    textAlign: "center",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+  },
+  textReclamo: {
+    color: "red",
+    fontWeight: "bold",
+    textAlignVertical: "top",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    height: 200,
+  },
+
+  fechaResolucionReclamo: {
+    color: "red",
+    fontWeight: "bold",
+    textAlignVertical: "top",
+    height: 40,
+  },
+
+  comentarioResolucionReclamo: {
+    color: "red",
+    fontWeight: "bold",
+    textAlignVertical: "top",
+    height: 100,
+  },
+
+  buttons: {
+    width: 150,
+    marginLeft: 22,
+  },
+
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+export default AprobarReclamoScreen;
