ef8e6c29496a1ea256e7308a24d102f4969d6c39
Sebastian Monti <smonti@mulesoft.com>
2020-11-19 02:07:25 -0300

    Se termina estileo de componente de detalle de Reclamo. Faltaria conectar con backend y manejar mensajes de aprobacion/error.

--- src/components/reclamos/detalle/DetalleReclamoScreen.js
+++ src/components/reclamos/detalle/DetalleReclamoScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { ScrollView, View } from 'react-native';
-import { ActivityIndicator, Card, Colors, Paragraph, Subheading, Title } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { ActivityIndicator, Button, Card, List, Subheading, Surface } from 'react-native-paper';
 import backendAdminConsorcios from '../../../apis/backendAdminConsorcios';
 
 const DetalleReclamoScreen = (props) => {
@@ -17,7 +18,7 @@ const DetalleReclamoScreen = (props) => {
                     "content-type": "application/json"
                 }
             }).then((response) => {
-                //console.log(response.data);
+                console.log(response.data);
                 if (mounted) {
                     setDetalleReclamo(response.data);
                     setIsLoading(false);
@@ -36,27 +37,95 @@ const DetalleReclamoScreen = (props) => {
     }, [props]);
 
     return (
-        <ScrollView style={{ flex: 1 }}>
-            <Card>
-                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-                {isLoading
-                    ? (
-                        <View style={{ flex: 1, alignItems: 'center' }}>
-                            <Title style={{ fontWeight: 'bold', textAlign: 'center' }}>Estamos recuperando el detalle de tu reclamo</Title>
-                            <ActivityIndicator animating={true} color={Colors.red800} size={"large"} />
+        <View style={{ flex: 1 }}>
+            <Surface style={styles.surface}>
+                <Card style={styles.card}>
+                    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                    {isLoading
+                        ? (
+                            <View style={{ flex: 1, alignItems: 'center', width: 350 }}>
+                                <Subheading style={styles.loadingInfo}>Estamos recuperando el detalle de tu reclamo</Subheading>
+                                <ActivityIndicator animating={true} size={"large"} />
+                            </View>
+                        )
+                        : (<View style={{ flex: 1, width: 350 }}>
+                            <Card.Title title={detalleReclamo.titulo} />
+                            <ScrollView>
+                                <Card.Content>
+                                    <List.Item
+                                        title={detalleReclamo.categoria}
+                                        left={props => <List.Icon {...props} icon="hammer" />}
+                                    />
+                                    <List.Item
+                                        title={detalleReclamo.edificio.calle + ' ' + detalleReclamo.edificio.altura + ', ' + detalleReclamo.edificio.barrio}
+                                        left={props => <List.Icon {...props} icon="home-city-outline" />}
+                                    />
+                                    <List.Item
+                                        title={detalleReclamo.descripcion}
+                                        titleNumberOfLines={4}
+                                        left={props => <List.Icon {...props} icon="clipboard-text-outline" />}
+                                    />
+                                    <List.Item
+                                        title="Estado"
+                                        description={detalleReclamo.estado}
+                                        left={props => <List.Icon {...props} icon="progress-wrench" />}
+                                    />
+                                    <List.Item
+                                        title="Fecha de creación"
+                                        description={detalleReclamo.fechaCreacion}
+                                        left={props => <List.Icon {...props} icon="calendar-month-outline" />}
+                                    />
+                                    <List.Item
+                                        title="Fecha comienzo de obras"
+                                        description={detalleReclamo.fechaComienzoObras || "No asignada"}
+                                        left={props => <List.Icon {...props} icon="calendar-clock" />}
+                                    />
+                                    <List.Item
+                                        title="Fecha de resolución"
+                                        description={detalleReclamo.fechaResolucion || "No finalizado"}
+                                        left={props => <List.Icon {...props} icon="calendar-check-outline" />}
+                                    />
+                                </Card.Content>
+                                <View style={styles.buttonContainer}>
+                                    <Button mode="contained" onPress={() => console.log("Aprobado")}>
+                                        Aprobar
+                                    </Button>
+                                    <Button mode="contained" onPress={() => console.log("Rechazado")}>
+                                        Rechazar
+                                    </Button>
+                                </View>
+                            </ScrollView>
                         </View>
-                    )
-                    : (<View style={{ flex: 1 }}>
-                        <Card.Title title={detalleReclamo.titulo} />
-                        <Card.Content>
-                            <Paragraph>{detalleReclamo.descripcion}</Paragraph>
-                        </Card.Content>
-                    </View>
-                    )
-                }
-            </Card>
-        </ScrollView>
+                        )
+                    }
+                </Card>
+            </Surface>
+        </View>
     );
 }
 
 export default DetalleReclamoScreen;
+
+const styles = StyleSheet.create({
+    loadingInfo: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 40
+    },
+    surface: {
+        padding: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 5,
+        backgroundColor: '#cfd3ce',
+    },
+    card: {
+        elevation: 10
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: 20
+    }
+});
