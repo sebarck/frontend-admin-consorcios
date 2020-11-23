3f404a3a5d4441b7eb533f86feec95064e94aba4
Sebastian Monti <smonti@mulesoft.com>
2020-11-23 19:20:51 -0300

    Mas arreglos en detalle de reclamo. Se arregla definitivamente el problema modificando el useEffect para que quede bien. Replicar lo mismo en demas.

--- src/apis/backendAdminConsorcios.js
+++ src/apis/backendAdminConsorcios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://backend-admin-consorcios.herokuapp.com/api-adminconsorcios/v1'
+    baseURL: 'https://backend-admin-consorcios.herokuapp.com/api-adminconsorcios/v1',
+    headers: {
+        "content-type": "application/json"
+    }
 });
